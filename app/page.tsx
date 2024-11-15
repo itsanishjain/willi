745d26742493d0ccff01a53f38adfb9aa66a005d
Anish Jain <43033153+itsanishjain@users.noreply.github.com>
2024-11-15 13:17:06 +0700

    add login wallet code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,49 @@
+// "use client";
+// import { useEffect } from "react";
+
+// export default function Page() {
+//   useEffect(() => {
+//     const dbOpenRequest = window.indexedDB.open("WALLET_CONNECT_V2_INDEXED_DB");
+//     console.log(dbOpenRequest);
+//   }, []);
+
+//   return <div className="flex h-screen"></div>;
+// }
+
 "use client";
-import { useEffect } from "react";
+import {
+  useAuthModal,
+  useLogout,
+  useSignerStatus,
+  useUser,
+} from "@account-kit/react";
 
-export default function Page() {
-  useEffect(() => {
-    const dbOpenRequest = window.indexedDB.open("WALLET_CONNECT_V2_INDEXED_DB");
-    console.log(dbOpenRequest);
-  }, []);
+export default function Home() {
+  const user = useUser();
+  const { openAuthModal } = useAuthModal();
+  const signerStatus = useSignerStatus();
+  const { logout } = useLogout();
 
-  return <div className="flex h-screen"></div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24 gap-4 justify-center text-center">
+      {signerStatus.isInitializing ? (
+        <>Loading...</>
+      ) : user ? (
+        <div className="flex flex-col gap-2 p-2">
+          <p className="text-xl font-bold">Success!</p>
+          You're logged in as {user.email ?? "anon"}.
+          <button
+            className="akui-btn akui-btn-primary mt-6"
+            onClick={() => logout()}
+          >
+            Log out
+          </button>
+        </div>
+      ) : (
+        <button className="akui-btn akui-btn-primary" onClick={openAuthModal}>
+          Login
+        </button>
+      )}
+    </main>
+  );
 }
