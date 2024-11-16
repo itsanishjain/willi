adf4fefa54e7c0f4d113ee1a300da381397bad32
Anish Jain <43033153+itsanishjain@users.noreply.github.com>
2024-11-17 01:42:50 +0700

    code cleanup

--- app/add-beneficiary/page.tsx
+++ app/add-beneficiary/page.tsx
@@ -1,14 +1,6 @@
 "use client";
 
 import AddBeneficiaryForm from "@/components/app/AddBeneficiaryForm";
-import { Button } from "@/components/ui/button";
-
-import {
-  useAuthModal,
-  useLogout,
-  useSignerStatus,
-  useUser,
-} from "@account-kit/react";
 import AuthWrapper from "@/components/app/AuthWrapper";
 
 export default function AddBeneficiaryPage() {
