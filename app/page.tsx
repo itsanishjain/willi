6ba4d465ddd38806d4eb1cf8a7bed7e1267c4544
Anish Jain <43033153+itsanishjain@users.noreply.github.com>
2024-11-16 17:26:15 +0700

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,7 @@
 import { Button } from "@/components/ui/button";
 import BeneficiaryList from "@/components/app/BeneficiaryList";
 import SubscriptionStatus from "@/components/app/SubscriptionStatus";
-import {
-  useAuthModal,
-  useLogout,
-  useSignerStatus,
-  useUser,
-} from "@account-kit/react";
-// import { useAuthenticate } from "@account-kit/react";
+import { useAuthModal, useSignerStatus, useUser } from "@account-kit/react";
 import { Loader } from "lucide-react";
 import { v4 } from "uuid";
 
@@ -19,21 +13,9 @@ export default function Home() {
   const user = useUser();
   const { openAuthModal, isOpen } = useAuthModal();
   const signerStatus = useSignerStatus();
-  const { logout } = useLogout();
-
-  // const { authenticate, authenticateAsync, isPending, error } = useAuthenticate(
-  //   {
-  //     // these are optional
-  //     onSuccess: () => {
-  //       // do something on success
-  //       console.log("successfully logged IN");
-  //     },
-  //     onError: (error) => console.error(error),
-  //   }
-  // );
 
   const addUser = async (body: any) => {
-    const response = await fetch("/api/user", {
+    fetch("/api/user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -43,19 +25,13 @@ export default function Home() {
   };
 
   useEffect(() => {
-    console.log("fuckdkfasdkfjmaskdjfkdsjfksd lasdjfoasdjfk asdkfj safjaskdj ");
     if (user && !isOpen) {
       const uid = v4();
       const body = { email: user.email, walletAddress: user.address, uid: uid };
       addUser(body);
       console.log(body);
-      window.localStorage.setItem("WILLI_UID", uid);
     }
   }, [user]);
-
-  // console.log("authenticateauthenticateauthenticate", isPending);
-
-  // console.log("auth errro", error);
 
   return (
     <>
