6bf964b17329673710ad134e9129ed9226f58dcf
Anish Jain <43033153+itsanishjain@users.noreply.github.com>
2024-11-17 01:42:15 +0700

    fixxx

--- app/add-beneficiary/page.tsx
+++ app/add-beneficiary/page.tsx
@@ -9,28 +9,12 @@ import {
   useSignerStatus,
   useUser,
 } from "@account-kit/react";
+import AuthWrapper from "@/components/app/AuthWrapper";
 
-import { Loader } from "lucide-react";
-
-export default function Home() {
-  const user = useUser();
-  const { openAuthModal, isOpen, closeAuthModal } = useAuthModal();
-  const signerStatus = useSignerStatus();
-  const { logout } = useLogout();
-
+export default function AddBeneficiaryPage() {
   return (
-    <>
-      {signerStatus.isInitializing ? (
-        <Loader className="animate-spin w-6 h-6" />
-      ) : user ? (
-        <div className="flex flex-col gap-2 p-2">
-          <AddBeneficiaryForm />
-        </div>
-      ) : (
-        <main className="flex min-h-screen flex-col items-center p-24 gap-4 justify-center text-center">
-          <Button onClick={openAuthModal}>Login</Button>
-        </main>
-      )}
-    </>
+    <AuthWrapper>
+      <AddBeneficiaryForm />
+    </AuthWrapper>
   );
 }
