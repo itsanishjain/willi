f1381f9b3420a6e2df67ba31108266c351e3d31a
Anish Jain <43033153+itsanishjain@users.noreply.github.com>
2024-11-17 16:01:09 +0700

    add connected user address for nft thing

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,13 +2,15 @@ import BankingDashboard from "@/components/app/BankingDashboard";
 import NFTPortfolio from "@/components/app/NFTPortfolio";
 import TokenPortfolio from "@/components/app/TokenPortfolio";
 import AuthWrapper from "@/components/app/AuthWrapper";
+import { useUser } from "@account-kit/react";
 
 export default function Page() {
+  const user = useUser();
   return (
     <AuthWrapper>
       <>
         <BankingDashboard />
-        <NFTPortfolio />
+        <NFTPortfolio walletAddress={user?.address || ""} />
         <TokenPortfolio walletAddress="0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045" />
       </>
     </AuthWrapper>
