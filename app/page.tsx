316f3feb5397e51cafda55d25f622561200db9cb
Anish Jain <43033153+itsanishjain@users.noreply.github.com>
2024-11-17 17:38:33 +0700

    fixed the main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,27 @@
 import { useAuthModal, useSignerStatus, useUser } from "@account-kit/react";
 import AuthWrapper from "@/components/app/AuthWrapper";
 import { Button } from "@/components/ui/button";
+import { Loader } from "lucide-react";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const user = useUser();
   const { openAuthModal } = useAuthModal();
+  const signerStatus = useSignerStatus();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/dashboard");
+    }
+  }, [user]);
+
+  if (signerStatus.isInitializing && user) {
+    return <Loader className="animate-spin w-6 h-6" />;
+  }
+
   return (
     <AuthWrapper>
       <div>Hello world</div>
