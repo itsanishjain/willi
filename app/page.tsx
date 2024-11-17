f03bdf17ede22e095fef294b94202c6c71c42a1e
Anish Jain <43033153+itsanishjain@users.noreply.github.com>
2024-11-18 02:21:33 +0700

    fixxxx

--- app/page.tsx
+++ app/page.tsx
@@ -28,26 +28,26 @@ export default function Home() {
     <AuthWrapper>
       <div className="flex flex-col items-center w-full h-full relative">
         <div className="w-full">
-          <img 
+          <img
             src="/willihome.svg"
             alt="Logo"
-            className="w-full h-auto" 
-            style={{ display: 'block' }}
+            className="w-full h-auto"
+            style={{ display: "block" }}
           />
         </div>
-        <Button 
-          onClick={openAuthModal} 
+        <Button
+          onClick={openAuthModal}
           className="absolute"
-          style={{ 
-            color: 'white',
-            backgroundColor: '#000000',
-            borderRadius: '42px',
-            top: '14%',
-            fontSize: '18px',
-            fontWeight: '400',
-            padding: '24px 28px',
-            transform: 'translateY(-50%)', // Centers the button vertically at its position
-            zIndex: 10 // Ensures button stays on top
+          style={{
+            color: "white",
+            backgroundColor: "#000000",
+            borderRadius: "42px",
+            top: "14%",
+            fontSize: "18px",
+            fontWeight: "400",
+            padding: "24px 28px",
+            transform: "translateY(-50%)", // Centers the button vertically at its position
+            zIndex: 10, // Ensures button stays on top
           }}
         >
           Create Will
